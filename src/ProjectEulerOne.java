e71e8e154a41dc093138e67162bdc539e46077a6
wnalle <nalle@hawaii.edu>
2015-02-01 13:09:53 -1000

    ProjectEulerOne

--- src/ProjectEulerOne.java
+++ src/ProjectEulerOne.java
@@ -2,4 +2,15 @@
  * Created by wgnalle on 2/1/2015.
  */
 public class ProjectEulerOne {
+  public static void main(String[] args) {
+    int sum = 0;
+
+    for (int i = 0; i < 1000; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        sum += i;
+      }
+    }
+
+    System.out.println("The sum is: " + sum);
+  }
 }
